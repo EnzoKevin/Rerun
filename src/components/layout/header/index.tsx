d8cdda282c3b79e57aa3de51369ac02c942d65cd
enzo Kevin <enzoclaro2021@gmail.com>
2025-06-20 21:45:16 -0300

    Header fixed

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,21 +1,23 @@
 import * as S from "./styles";
+import Logo from "@/components/images/logo.png";
 
 export default function Header() {
   return (
     <S.Header>
       <figure>
-        <span>Icone</span>
+        <span style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+          <img src={Logo.src} alt="" /> Rerun
+        </span>
       </figure>
       <nav>
         <S.ul>
-          <li>Home</li>
+          <li>Careers</li>
+          <li>Examples</li>
           <li>Docs</li>
-          <li>Outro</li>
-          <li>Outro</li>
-          <li>Outro</li>
+          <li>Blog</li>
         </S.ul>
       </nav>
-      <button>SearchBar</button>
+      {<button style={{ visibility: "hidden" }}>SearchBar</button>}{" "}
     </S.Header>
   );
 }
